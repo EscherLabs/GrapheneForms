18065153f6a2988f081f93fff237e83054daaad0
Adam Smallcomb <atsmallcomb@gmail.com>
2019-03-14 16:26:17 -0400

    generate docs

--- make.sh
+++ make.sh
@@ -3,11 +3,14 @@ cat src/js/gform.js src/js/types.js src/js/conditions.js src/js/validate.js > bi
 minify src/js/gform.js src/js/types.js src/js/conditions.js src/js/validate.js > bin/gform.engine.min.js 
 minify bin/gform.engine.min.js src/js/themes/default.js > bin/gform.min.js 
 minify bin/gform.engine.min.js src/js/themes/bootstrap.js > bin/gform_bootstrap.min.js 
-minify bin/gform.engine.min.js src/js/themes/bulma.js > bin/gform_bulma.min.js 
-minify bin/gform.engine.min.js > bin/gform_skeleton.min.js 
-minify bin/gform.engine.min.js src/js/themes/milligram.js > bin/gform_milligram.min.js
-minify src/js/themes/default.js > bin/default.theme.min.js
+# minify src/js/themes/default.js > bin/default.theme.min.js
+# minify src/js/themes/bootstrap.js > bin/bootstrap.theme.min.js
+# minify bin/gform.engine.min.js src/js/themes/bulma.js > bin/gform_bulma.min.js 
+# minify bin/gform.engine.min.js src/js/themes/skeleton.js> bin/gform_skeleton.min.js 
+# minify bin/gform.engine.min.js src/js/themes/milligram.js > bin/gform_milligram.min.js
 cp src/js/themes/* bin/
 
 cp bin/* docs/assets/js/
 sass src/scss/gform.scss > docs/assets/css/gform.css 
+
+node ~/Projects/charge/charge.js src/_site/
